21c9e8c43a0db97082123997600f332687bb75a9
samuelsegal <samuelmosessegal@gmail.com>
2020-04-18 14:30:19 -0400

    front end boiled and plated

---
+++ stream-client/src/components/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import StreamList from './streams/StreamList';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamShow from './streams/StreamShow';
+import StreamDelete from './streams/StreamDelete';
+import Header from './Header';
+
+const app = () => {
+	return (
+		<div className="ui container">
+			<BrowserRouter>
+				<div>
+					<Header />
+					<Route path="/" exact component={StreamList} />
+					<Route path="/streams/create" exact component={StreamCreate} />
+					<Route path="/streams/edit" exact component={StreamEdit} />
+					<Route path="/streams/show" exact component={StreamShow} />
+					<Route path="/streams/delete" exact component={StreamDelete} />
+				</div>
+			</BrowserRouter>
+		</div>
+	);
+};
+
+export default app;
